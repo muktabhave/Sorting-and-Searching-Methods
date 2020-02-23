0495c7125d3ba8cd8d67b1b29a30b6adc81a9d46
Mukta Girkar <muktagirkar@gmail.com>
2020-02-22 16:52:57 -0800

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,27 +1,17 @@
 def BubbleSort(a):
-
-    n= len(a)-1
-
+    
+    n=len(a)-1
+    
     for i in range(n):
-
-        for i in range (n-i):
-
-            if (a[i]> a[i+1]):
-
-                temp= a[i+1]
-                a[i+1]= a[i]
-                a[i]= temp
+        
+        for j in range(n-i):
+            if(a[j]>a[j+1]):
+                temp=a[j]
+                a[j]=a[j+1]
+                a[j+1]=temp
 
     return a
 
-def main():
-
-    print(BubbleSort([10,9,8,7,6,2]))
-
-if( __name__=="__main__"):
-
-    main()
-
-
-
-
+if (__name__=="__main__"):
+    
+    print(BubbleSort([2,10,5,8,3]))
